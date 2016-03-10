f3fb06cc6135d5f40e2a1eb79ea516448beee599
wagnst <mail@steffenwagner.com>
2016-03-10 20:57:00 +0100

    first try for job template

--- manifests/director/job.pp
+++ manifests/director/job.pp
@@ -1,4 +1,25 @@
 
-define bareos::director::job ($name = $title) {
-
+define bareos::director::job (
+    $job_name = $title,
+    $job_definition,
+    $job_type,
+    $level,
+    $file_set,
+    $storage,
+    $schedule,
+    $pool,
+    $messages,
+    $where,
+    $priority,
+    $run_before_script,
+    $run_after_script,
+) {
+	include params
+	include director
+	
+	concat::fragment {"${director::jobs_conf}+${job_name}":
+		target  => $director::jobs_conf,
+		order   => '05',
+		content => template('bareos/director/job.conf.erb'),
+	}
 }
