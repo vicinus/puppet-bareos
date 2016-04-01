93e07e942527c4e6ff2052f050f18eaa015c7da8
Phillip Schichtel <phillip.public@schich.tel>
2016-04-01 14:59:32 +0200

    forgot to override the architecture

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -20,11 +20,12 @@ class bareos::repo (
             
                 # puppetlabs-apt 2.x is not able to specify an empty release, so directly create the apt::setting resource here
             
-                $setting  = "list-${global::repo_name}"
-                $_include = {'deb' => true}
-                $location = "${bareos_repo_base}/${repo_path}/${::operatingsystem}_${::operatingsystemmajrelease}.0"
-                $repos    = '/'
-                $comment  = $global::repo_name
+                $setting      = "list-${global::repo_name}"
+                $_include     = {'deb' => true}
+                $location     = "${bareos_repo_base}/${repo_path}/${::operatingsystem}_${::operatingsystemmajrelease}.0"
+                $repos        = '/'
+                $comment      = $global::repo_name
+                $architecture = false
                 
                 ::apt::key {'0143857D9CE8C2D182FE2631F93C028C093BFBA2':}
                 
--- manifests/repo/contrib.pp
+++ manifests/repo/contrib.pp
@@ -10,11 +10,12 @@ class bareos::repo::contrib {
             
                 # puppetlabs-apt 2.x is not able to specify an empty release, so directly create the apt::setting resource here
             
-                $setting  = "list-${global::contrib_repo_name}"
-                $_include = {'deb' => true}
-                $location = "${bareos_repo_base}/${repo_path}/${::operatingsystem}_${::operatingsystemmajrelease}.0"
-                $repos    = '/'
-                $comment  = $global::contrib_repo_name
+                $setting      = "list-${global::contrib_repo_name}"
+                $_include     = {'deb' => true}
+                $location     = "${bareos_repo_base}/${repo_path}/${::operatingsystem}_${::operatingsystemmajrelease}.0"
+                $repos        = '/'
+                $comment      = $global::contrib_repo_name
+                $architecture = false
                 
                 ::apt::key {'0143857D9CE8C2D182FE2631F93C028C093BFBA2':}
                 
